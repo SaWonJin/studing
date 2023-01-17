72fff33f864bb01b50c0f6b075c7578d91bb4346
SaWonJin <sa6118@naver.com>
2023-01-17 13:48:47 +0900

    first commit

---
+++ backjun/src/backjun/back9.java
@@ -0,0 +1,34 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class back9 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int k =1 ;
+		int q =1 ;
+		int r =2 ;
+		int b =2 ;
+		int kn =2 ;
+		int p=8 ;
+		
+		k = k - sc.nextInt();
+		q = q - sc.nextInt();
+		r = r - sc.nextInt();
+		b = b - sc.nextInt();
+		kn = kn - sc.nextInt();
+		p = p - sc.nextInt();
+		
+		System.out.print(k+" ");
+		System.out.print(q+" ");
+		System.out.print(r+" ");
+		System.out.print(b+" ");
+		System.out.print(kn+" ");
+		System.out.print(p+" ");
+		
+	}
+
+}
---
+++ backjun/src/backjun/test.java
@@ -0,0 +1,9 @@
+package backjun;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
