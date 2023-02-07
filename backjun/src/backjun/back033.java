5fbec02d10ffa64f4cbd883e6d4c7e6f24f6025d
SaWonJin <sa6118@naver.com>
2023-02-07 15:47:55 +0900

    20230207commit

---
+++ backjun/src/backjun/back033.java
@@ -0,0 +1,27 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class back033 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+
+//		while(a==a) {
+//			
+//			
+//		}
+		
+	}
+
+}
---
+++ backjun/src/backjun/back034.java
@@ -0,0 +1,23 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class back034 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a,b;
+		a = sc.nextInt();
+		
+		int[] c =new int[a] ;
+		for(int i =0; i<a; i++) {
+			
+			c[i] = sc.nextInt(); 
+			
+		}
+
+	}
+
+}
